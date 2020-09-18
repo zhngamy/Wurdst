98cc98adc60f6862ec1bc0f7d3a6ce283a9449dc
Amy Zheng <awlzhng@gmail.com>
2020-09-18 14:22:26 -0700

    Populate DictionaryController with fake data

---
+++ Wurdst/Controllers/DictionaryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wurdst.Models;
+using Wurdst.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wurdst.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            DateTime now = DateTime.Now;
+
+            Word word = new Word()
+            {
+                Definition = "Example definition",
+                SentenceUsage = "Example sentence",
+                WhenCreated = now
+            };
+
+            Tag tag = new Tag()
+            {
+                TagName = "Biology",
+                WhenCreated = now
+            };
+
+            //Create the new viewmodel
+            DictionaryIndexViewModel vm = new DictionaryIndexViewModel()
+            {
+                Word = word,
+                Tag = tag
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Wurdst/Models/ViewModels/DictionaryIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Wurdst.Models;
+
+namespace Wurdst.Models.ViewModels
+{
+    public class DictionaryIndexViewModel
+    {
+        public Word Word { get; set; }
+
+        public Tag Tag { get; set; }
+
+        public WordTag WordTag { get; set; }
+
+
+        //public IEnumerator<Word> Words { get; set; }
+
+        //public IEnumerator<Tag> Tags { get; set; }
+
+        //public IEnumerator<WordTag> WordTags { get; set; }
+    }
+}
